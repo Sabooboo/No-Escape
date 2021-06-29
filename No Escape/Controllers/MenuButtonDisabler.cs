be51edfaba37157647f2d5e7f37b5dde07362417
Sabooboo <smjdunne@gmail.com>
2021-06-29 15:57:55 -0300

    Add project files.

---
+++ No Escape/Controllers/MenuButtonDisabler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+using UnityEngine.UI;
+using IPA.Utilities;
+using UnityEngine;
+using HMUI;
+
+namespace No_Escape.Configuration.Controllers
+{
+    class MenuButtonDisabler : IInitializable, IDisposable
+    {
+
+        private readonly PauseController pauseController;
+        private PauseMenuManager pauseMenuManager;
+
+        public MenuButtonDisabler(PauseController pauseController, PauseMenuManager pauseMenuManager)
+        {
+            this.pauseController = pauseController;
+            this.pauseMenuManager = pauseMenuManager;
+        }
+
+        public void Initialize()
+        {
+            pauseController.didPauseEvent += PauseController_didPauseEvent;
+        }
+
+        private void PauseController_didPauseEvent()
+        {
+            DisableMenu();
+        }
+
+        private void DisableMenu()
+        {
+            // Somewhere right now PixelBoom is dying reading this. this is not good code. if you want to see Pog and Epic code for button disables look at PauseChamp :D
+            Transform buttons = pauseMenuManager.transform.Find("Wrapper").transform.Find("MenuWrapper").transform.Find("Canvas").transform.Find("MainBar").transform.Find("Buttons");
+            GameObject menuButton = buttons.GetChild(0).gameObject;
+            menuButton.GetComponent<NoTransitionsButton>().interactable = false;
+
+
+        }
+
+        public void Dispose()
+        {
+            pauseController.didPauseEvent -= PauseController_didPauseEvent;
+        }
+    }
+}
---
+++ No Escape/Installers/PauseMenuInstaller.cs
@@ -0,0 +1,18 @@
+﻿using No_Escape.Configuration.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace No_Escape.Installers
+{
+    class PauseMenuInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<MenuButtonDisabler>().AsSingle();
+        }
+    }
+}
